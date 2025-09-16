821bad014cba6c9cd0233539492632d9c0526735
Nangee <139064476+Nangeerang@users.noreply.github.com>
2025-09-17 01:39:45 +0700

    ex00,01

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,9 @@
+def main():
+    
+    number = int(input("Type for Number : "))
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+
+main()
